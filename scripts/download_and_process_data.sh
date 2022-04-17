75358ca2283ac3f5bc5b81f13e37a50ce08bdf86
Arda Arslan <aarslan@ethz.ch>
2022-04-17 18:05:14 +0200

    Speed up data processing

--- scripts/download_and_process_data.sh
+++ scripts/download_and_process_data.sh
@@ -1,6 +1,6 @@
  #!/bin/bash
 
- # Reading arguments and mapping to respective variables
+ # Read arguments and map to respective variables
  while [ $# -gt 0 ]; do
    if [[ $1 == *"--"* ]]; then
         v="${1/--/}"
@@ -17,9 +17,9 @@ mkdir $FACE_DIR
 python3 face_data_downloader.py -d compressed -o $DATASETS_DIR --not_altered --not_mask
 mv $DATASETS_DIR/FaceForensics_compressed/* $FACE_DIR; rm -rf $DATASETS_DIR/FaceForensics_compressed
 mkdir $FACE_DIR/validation; mkdir $FACE_DIR/validation/original; mv $FACE_DIR/val/original/* $FACE_DIR/validation/original; rm -rf $FACE_DIR/val
-bsub -n 1 -W 24:00 -R "rusage[mem=8192]" python face_data_processor.py --videos_dir $FACE_DIR --split train
-bsub -n 1 -W 24:00 -R "rusage[mem=8192]" python face_data_processor.py --videos_dir $FACE_DIR --split validation
-bsub -n 1 -W 24:00 -R "rusage[mem=8192]" python face_data_processor.py --videos_dir $FACE_DIR --split test
+bsub -n 4 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=10240]" python face_data_processor.py --videos_dir $FACE_DIR --split train
+bsub -n 4 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=10240]" python face_data_processor.py --videos_dir $FACE_DIR --split validation
+bsub -n 4 -W 24:00 -R "rusage[mem=8192, ngpus_excl_p=1]" -R "select[gpu_mtotal0>=10240]" python face_data_processor.py --videos_dir $FACE_DIR --split test
 
 wget -O $DATASETS_DIR/body.zip "https://www.dropbox.com/s/coapl05ahqalh09/smplpix_data_test_final.zip?dl=1"
 unzip $DATASETS_DIR/body.zip -d $DATASETS_DIR/body_smplpix_temp
